4d7cb41e9d90dbe8f8ef607b1291a28750c1a1ad
Sébastien L'haire <sebastien@lhaire.org>
2021-01-12 22:55:40 +0100

    web route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\PaginatorController;
+use \App\Http\Controllers\DaterangepickerController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes

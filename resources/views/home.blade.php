b0bff310d3162effc8bfbddf661075222e14412e
Sébastien L'haire <sebastien@lhaire.org>
2021-01-12 19:04:57 +0100

    controller and views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title', 'Home')
+@section('content')
+<div class="jumbotron">
+  <h1 class="display-4">Seb's Laravel Package Demo Site</h1>
+  <p class="lead">My name is <a href="https://sebastien.lhaire.org">Sébastien L'haire</a>. I work as web developper.
+  I developp <a target="_blank" href="https://laravel.com">Laravel packages</a>
+  for the personal sites I build.</p>
+  <p>These packages are freely available for the web community on <a target="_blank" href="https://packagist.org/packages/seblhaire/">Packagist</a>.
+  Source files are available on <a target="_blank" href="https://github.com/seblhaire/">GitHub</a>. You can fork the projects to adapt them to your needs,
+  notify bugs or propose merge requests. Translation files in your own language are very welcome.</p>
+</div>
+
+@endsection
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,19 @@
+<!-- Fixed navbar -->
+<header id="header">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <a class="navbar-brand" href="{{ \Config::get('app.url')}}">Sébastien L'haire - Laravel packages demo site</a>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+    @foreach (config('menu') as $idx => $item)
+      <li id="{!! $idx !!}" class="nav-item{!! $idx == $menu ? ' active' : '' !!}"><a class="nav-link" href="{!! $item['target'] !!}" title="{{ isset($item['icon']) ? $item['title'] : '' }}">
+      @if (isset($item['icon']))
+      	{!! $item['icon'] !!}
+      @else
+      	{{ $item['title'] }}
+      @endif
+      </a></li>
+    @endforeach
+    </ul>
+  </div>
+</nav>
+</header>

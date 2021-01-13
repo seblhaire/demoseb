191fc49c51d6cb69dd82842f6a2bcf61454d91c8
Sébastien L'haire <sebastien@lhaire.org>
2021-01-13 20:56:16 +0100

    helpers

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-
+    if ($( window ).width() > 767){
+        jQuery(window).scroll(function () {
+            if ($(this).scrollTop() > 45) {
+              jQuery('#back-to-top').fadeIn();
+            } else {
+               jQuery('#back-to-top').fadeOut();
+            }
+        });
+    }else{
+        jQuery(window).scroll(function () {
+            if ($(this).scrollTop() > 45) {
+                jQuery('#back-to-top').fadeIn();
+            } else {
+                jQuery('#back-to-top').fadeOut();
+            }
+        });
+    }
+       // scroll body to 0px on click
+    jQuery('#back-to-top').click(function () {
+        jQuery('#back-to-top').tooltip('hide');
+        jQuery('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 });

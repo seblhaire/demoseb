e851ca7787823752c194ff20e39fae2c5d55b586
Sébastien L'haire <sebastien@lhaire.org>
2021-05-02 00:35:30 +0200

    bugfix

--- app/Http/Controllers/UploaderController.php
+++ app/Http/Controllers/UploaderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Seblhaire\Uploader\UploaderHelper;
+use Illuminate\Support\Facades\Storage;
 
 class UploaderController extends Controller
 {

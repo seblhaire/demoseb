0cef0998f84d0371f6750fdc08f9dc363fbd054b
Sébastien L'haire <sebastien@lhaire.org>
2021-05-01 23:46:59 +0200

    js packages add to webpack source

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,5 +6,8 @@ window.jQuery = jQuery;
 require('bootstrap');
 import './custom.js';
 global.moment = require('moment');
+global.Clipboard = require('clipboard');
 require('daterangepicker');
 require('../../vendor/seblhaire/tablebuilder/resources/js/tablebuilder.js');
+
+require('../../vendor/seblhaire/uploader/resources/js/upload.js');

c86e32dce517e14c1409ce27aa5abab75fcd1c6b
Sébastien L'haire <sebastien@lhaire.org>
2021-05-14 09:38:35 +0200

    humm

--- resources/views/autocompleter.blade.php
+++ resources/views/autocompleter.blade.php
@@ -3,4 +3,7 @@
 @section('title', 'Autocompleter')
 @section('content')
 <h1>Autocompleter</h1>
+<div class="form-group">
+  <input type="text" id="autocompletetest" class="form-control col-md-3" name="autocompletetest"/>
+</div>
 @endsection

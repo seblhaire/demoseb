03602d9461bbc87a7c700164ffd0500effeafa6d
Sébastien L'haire <sebastien@lhaire.org>
2021-08-14 22:59:23 +0200

    add debugbar

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Seblhaire\Autocompleter\AutocompleterServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class
 
     ],
 
@@ -229,7 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'AutocompleterHelper' => Seblhaire\Autocompleter\AutocompleterHelper::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 

c9254e476a1d4613335e3d00627c0fd390c2c18b
Sebastien L'haire <sebastien@lhaire.org>
2022-07-05 21:18:38 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,10 @@ use \App\Http\Controllers\DaterangepickerController;
 use \App\Http\Controllers\TablebuilderController;
 use \App\Http\Controllers\UploaderController;
 use \App\Http\Controllers\AutocompleterController;
-use Seblhaire\Uploader\FileuploadController;
+use \App\Http\Controllers\FileUploadController;
 use \App\Http\Controllers\TagsinputController;
+use \App\Http\Controllers\FormsbootstrapController;
+use \App\Http\Controllers\MenuUtilsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,20 +26,24 @@ Route::get('/cv', [HomeController::class, 'cv'])->name('cv');
 Route::get('/publis', [HomeController::class, 'publis'])->name('publis');
 Route::get('/paginator/{paginatortype?}/{param1?}/{param2?}', [PaginatorController::class, 'index'])->name('paginator');
 Route::get('/daterangepicker/{type?}/{lang?}', [DaterangepickerController::class, 'index'])->name('daterangepicker');
-Route::get('/tablebuilder', [TablebuilderController::class, 'index'])->name('tablebuilder');
-Route::get('/uploader', [UploaderController::class, 'index'])->name('uploader');
-Route::get('/autocompleter', [AutocompleterController::class, 'index'])->name('autocompleter');
-Route::get('/' . env('UPLOAD_DEL_URL'), [UploaderController::class, 'deleteFiles'])->name('uploader');
-Route::get('/tagsinput', [TagsinputController::class, 'index'])->name('tagsinput');
-
+Route::get('/tablebuilder/{type?}', [TablebuilderController::class, 'index'])->name('tablebuilder');
+Route::get('/uploader/{type?}', [UploaderController::class, 'index'])->name('uploader');
+Route::get('/autocompleter/{type?}', [AutocompleterController::class, 'index'])->name('autocompleter');
+Route::get('/formsbootstrap/{type?}', [FormsbootstrapController::class, 'index'])->name('formsbootstrap');
+Route::get('/' . env('UPLOAD_DEL_URL'), [UploaderController::class, 'deleteFiles'])->name('deletefiles');
+Route::get('/tagsinput/{type?}', [TagsinputController::class, 'index'])->name('tagsinput');
+Route::get('/menuutils/{type?}', [MenuUtilsController::class, 'index'])->name('menuutils');
 
 Route::post('/tablebuilder/loadtab', [TablebuilderController::class, 'loadTable'])->name('tableload');
 Route::post('/tablebuilder/loadtab2', [TablebuilderController::class, 'loadTable2'])->name('tableload2');
-Route::post('/processfile', [UploaderController::class, 'processFile'])->name('processfile');
-Route::post('/fileupload', [FileuploadController::class, 'index'])->name('fileupload');
+Route::post('/fileupload2', [FileUploadController::class, 'processFile'])->name('fileupload2');
+Route::post('/fileupload', [FileUploadController::class, 'index'])->name('fileupload');
 Route::post('/autocompleter/search', [AutocompleterController::class, 'search'])->name('autocompletesearch');
 Route::post('/tagsinput/search', [TagsinputController::class, 'search'])->name('tagsinputsearch');
 Route::post('/tagsinput/addemployee', [TagsinputController::class, 'addEmployee'])->name('tagsinputaddemployee');
+Route::post('/formsbootstrap/processform', [FormsbootstrapController::class, 'processform'])->name('formsbootstrap_processform');
+Route::post('/formsbootstrap/filldata', [FormsbootstrapController::class, 'filldata'])->name('formsbootstrap_filldata');
+Route::post('/formsbootstrap/checkoldpass', [FormsbootstrapController::class, 'checkoldpass'])->name('formsbootstrap_checkoldpass');
 Route::get('/refresh-csrf', function(){
     return csrf_token();
 })->name('refreshcsrf');

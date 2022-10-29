a866171e9f1c2d02ae560ef3bd0c89e68d0975f4
Sebastien L'haire <sebastien@lhaire.org>
2022-10-29 19:00:24 +0200

    js definition

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@ var jQuery = global.JQuery;
 window.$ = $;
 window.jQuery = jQuery;
 require('bootstrap');
-import './custom.js';
-global.moment = require('moment');
 global.Clipboard = require('clipboard');
+
+global.moment = require('moment');
 require('daterangepicker');
 require('../../vendor/seblhaire/daterangepickerhelper/resources/js/sebdaterangepicker.js');
 require('../../vendor/seblhaire/tablebuilder/resources/js/tablebuilder.js');
@@ -15,3 +15,5 @@ require('../../vendor/seblhaire/autocompleter/resources/js/autocompleter.js');
 require('../../vendor/seblhaire/tagsinput/resources/js/tagsinput.js');
 require('../../vendor/seblhaire/formsbootstrap/resources/js/seb.formsbootstrap.js');
 require('../../vendor/seblhaire/formsbootstrap/resources/js/jquery.richtext.js');
+require('./custom.js');
+require('../../vendor/seblhaire/uploader/resources/js/UploadedFileContainerExt.js');

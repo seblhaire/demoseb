6131388a1653c9e200e99852dcef5479d1d229cb
Sebastien L'haire <sebastien@lhaire.org>
2022-11-20 12:04:12 +0100

    fix formsbootstrap

--- resources/views/tags/ex1.blade.php
+++ resources/views/tags/ex1.blade.php
@@ -41,7 +41,7 @@ jQuery('#tagzone_addbtn').on('click', function(){
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title"  id="exampleModalLabel">Add employee</h5>
+        <h5 class="modal-title"  id="addEmployeeLabel">Add employee</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">

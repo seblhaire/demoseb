1926d0744727c63c0a02054cd2e3697e9f2c86eb
Sébastien L'haire <sebastien@lhaire.org>
2024-03-13 20:02:17 +0100

    laravel 11

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,20 @@
     <link href="{{ mix('css/app.css')}}" rel="stylesheet"/>
   </head>
   <body>
-    @include('navbar')
+    <!-- Fixed navbar -->
+    <header id="header" class="navbar navbar-expand-md navbar-light fixed-top">
+    <nav class="container flex-wrap flex-md-nowrap">
+        <a class="navbar-brand" href="{{ \Config::get('app.url')}}">Sébastien L'haire</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          {!! $mainmenu !!}
+          <hr class="d-md-none text-white-50">
+          {!! $rightmenu !!} 
+        </div>
+    </nav>
+    </header>
     <main role="main" class="container">
         @yield('content')
     </main> <!-- /container -->
